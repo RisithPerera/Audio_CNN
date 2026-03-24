b49b4605184a787ce0aa3ab725402ff7ed1068f3
RisithPerera <risithperera@eng.pdn.ac.lk>
2026-03-24 14:09:27 +0100

    Added Inference Process

--- inference.py
+++ inference.py
@@ -38,3 +38,52 @@ class AudioProcessor:
         return spectrogram.unsqueeze(0)
 
 
+class InferenceRequest(BaseModel):
+    audio_data: str
+
+
+class AudioClassifier:
+
+    def __init__(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        checkpoint = torch.load('models/audio_cnn.pth', map_location=self.device)
+        self.classes = checkpoint['classes']
+        self.model = AudioCNN(num_classes=len(self.classes))
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.model.to(self.device)
+        self.model.eval()
+
+        self.audio_processor = AudioProcessor()
+        print('Model Loaded')
+
+    @app.post("/inference/")
+    def inference(self, request: InferenceRequest):
+        audio_byte = base64.b64decode(request.audio_data)
+
+        audio_data, sample_rate = sf.read(io.BytesIO(audio_byte), dtype = "float32")
+
+        if audio_data.ndim > 1:
+            audio_data = np.mean(audio_data, axis=1)
+
+        if sample_rate != 22050:
+            audio_data = librosa.resample(audio_data, orig_sr=sample_rate, target_sr=22050)
+
+        spectrogram = self.audio_processor.process_audio_chunk(audio_data)
+        spectrogram = spectrogram.to(self.device)
+
+        with torch.no_grad():
+            output = self.model(spectrogram)
+            output = torch.nan_to_num(output)
+
+            probabilities = torch.softmax(output, dim=0)
+
+            top3_probs, top3_indices = torch.topk(probabilities[0], 3)
+
+            predictions = [{"class": self.classes[idx], "confidence": prob.item()}
+                           for prob, idx in zip(top3_probs, top3_indices)]
+
+            response = {
+                "predictions": predictions
+            }
+
+            return response
